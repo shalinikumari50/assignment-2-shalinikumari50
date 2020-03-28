6992b9f988c2d2b1da5cc4032cf5aa5fe905e28f
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 17:09:31 +0530

    add static method testCases(testCases int[][])(void) to do leftChildTraversal of different binary search trees

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,6 +7,8 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
         int[][] testCases = {{50, 30, 20, 40, 70, 60, 80},
@@ -14,6 +16,19 @@ public class MyMain {
                 {},
                 {90, 80, 70, 60, 50, 45, 40},
                 {50, 44, 60, 48, 55, 70, 32, 80, 61, 29, 18, 62, 63, 64, 65, 66}};
+
+    }
+
+    private static void testCases(int[][] testCases) {
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
+        int i = 0;
+        for (int[] test : testCases) {
+            i++;
+            System.out.println("Test " + i + ":");
+            myBinarySearchTree.createBST(test);
+            myBinarySearchTree.leftChildTraversal();
+            System.out.println("***************************************************");
+        }
 
     }
 }
