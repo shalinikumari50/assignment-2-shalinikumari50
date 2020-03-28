353471dff3ca69224210438c4445270298568b94
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 16:37:23 +0530

    add parameterized constructor: TreeNode(data:int)

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -10,4 +10,10 @@ public class TreeNode {
     private int data;
     private TreeNode left;
     private TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
