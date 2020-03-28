3b908f950b48dc5d00f873cd4e5d6b8e93628592
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 18:21:18 +0530

    add getter and setter methods

--- src/problem3/element/Element.java
+++ src/problem3/element/Element.java
@@ -8,4 +8,20 @@ public class Element<T> {
         this.data = data;
         this.priority = priority;
     }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
