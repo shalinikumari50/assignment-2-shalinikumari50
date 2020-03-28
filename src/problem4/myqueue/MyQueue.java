387390b541205b077a4acefc908ee22666ed9310
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 19:05:38 +0530

    add getter and setter methods

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -20,6 +20,27 @@ public class MyQueue {
         size = 0;
     }
 
+    public Node getStart() {
+        return start;
+    }
 
+    public void setStart(Node start) {
+        this.start = start;
+    }
 
+    public Node getEnd() {
+        return end;
+    }
+
+    public void setEnd(Node end) {
+        this.end = end;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
