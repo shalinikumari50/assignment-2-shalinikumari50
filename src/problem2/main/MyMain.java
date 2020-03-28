4c58b0566f9c2dc681d3abc69aa20c06041eb21b
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 17:37:07 +0530

    add two dimensional array: testCasesA(int[][]) containing the testCases for statement A

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -8,4 +8,9 @@ package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    int[][] testCasesA = {{50, 30, 20, 40, 70, 60, 80},
+            {50, 60, 70, 80, 90, 99, 897, 899},
+            {},
+            {90, 80, 70, 60, 50, 45, 40},
+            {50, 44, 60, 48, 55, 70, 32, 80, 61, 29, 18, 62, 63, 64, 65, 66}};
 }
