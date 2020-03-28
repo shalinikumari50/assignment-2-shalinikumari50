ace8568f9cecc0a43806c18cfd98ea2e601c0c38
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 17:21:21 +0530

    add method: preOrderTraversal(root:TreeNode, result:ArrayList<Integer>)(void) to find the preOrder traversal of the bst

--- src/problem2/traversals/Traversals.java
+++ src/problem2/traversals/Traversals.java
@@ -1,4 +1,16 @@
 package problem2.traversals;
 
+import problem1.node.TreeNode;
+
+import java.util.ArrayList;
+
 public class Traversals {
+    private void preOrderTraversal(TreeNode root, ArrayList<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.getData());
+        preOrderTraversal(root.getLeft(), result);
+        preOrderTraversal(root.getRight(), result);
+    }
 }
