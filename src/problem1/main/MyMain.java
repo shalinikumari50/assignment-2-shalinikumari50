0bba7c2fcf2d5c07493041295f78fcba2442b95e
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 17:05:12 +0530

    add two dimensional array: testCases(int[][])

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,4 +8,12 @@ package problem1.main;
 // executable class
 
 public class MyMain {
+    public static void main(String[] args) {
+        int[][] testCases = {{50, 30, 20, 40, 70, 60, 80},
+                {50, 60, 70, 80, 90, 99, 897, 899},
+                {},
+                {90, 80, 70, 60, 50, 45, 40},
+                {50, 44, 60, 48, 55, 70, 32, 80, 61, 29, 18, 62, 63, 64, 65, 66}};
+
+    }
 }
