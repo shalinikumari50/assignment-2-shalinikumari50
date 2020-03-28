fd3068433de2779459e02c453c634b63db96a14d
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 19:10:38 +0530

    add method: enqueue(newNode:Node<T>)(void) to insert new node in queue

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -9,9 +9,9 @@ package problem4.myqueue;
 
 import problem5.node.Node;
 
-public class MyQueue {
-    private Node start;
-    private Node end;
+public class MyQueue<T> {
+    private Node<T> start;
+    private Node<T> end;
     private int size;
 
     public MyQueue() {
@@ -20,19 +20,19 @@ public class MyQueue {
         size = 0;
     }
 
-    public Node getStart() {
+    public Node<T> getStart() {
         return start;
     }
 
-    public void setStart(Node start) {
+    public void setStart(Node<T> start) {
         this.start = start;
     }
 
-    public Node getEnd() {
+    public Node<T> getEnd() {
         return end;
     }
 
-    public void setEnd(Node end) {
+    public void setEnd(Node<T> end) {
         this.end = end;
     }
 
@@ -43,4 +43,17 @@ public class MyQueue {
     public void setSize(int size) {
         this.size = size;
     }
+
+    public void enqueue(Node<T> newNode) {
+        if (start == null) {
+            start = newNode;
+        } else {
+            end.setNext(newNode);
+        }
+        end = newNode;
+        size++;
+    }
+
+
+
 }
