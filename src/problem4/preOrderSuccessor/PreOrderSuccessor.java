c2877e590a605d4bcee5c076579989ede23c98dd
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 19:38:20 +0530

    add getter method: getMyQueue

--- src/problem4/preOrderSuccessor/PreOrderSuccessor.java
+++ src/problem4/preOrderSuccessor/PreOrderSuccessor.java
@@ -3,9 +3,17 @@ package problem4.preOrderSuccessor;
 import problem4.myqueue.MyQueue;
 
 public class PreOrderSuccessor {
-    private MyQueue myQueue;
+    private MyQueue<Integer> myQueue;
 
     public PreOrderSuccessor() {
-        this.myQueue = new MyQueue();
+        this.myQueue = new MyQueue<>();
     }
+
+    public MyQueue getMyQueue() {
+        return myQueue;
+    }
+
+
+
+
 }
