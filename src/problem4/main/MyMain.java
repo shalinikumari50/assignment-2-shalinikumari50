b379c4dc58a694266becb7ac6300e1cbb5002d5e
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 19:44:27 +0530

    add object:preOrderSuccessor of class: PreOrderSuccessor

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,12 +7,14 @@
 package problem4.main;
 
 import problem1.mybst.MyBinarySearchTree;
+import problem4.preOrderSuccessor.PreOrderSuccessor;
 
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
+        PreOrderSuccessor preOrderSuccessor = new PreOrderSuccessor();
     }
 
 }
