f087c519ca81c0c477cd0f176e36357209419e3f
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 18:20:48 +0530

    add parameterized constructor: Element(data:T, priority:int)

--- src/problem3/element/Element.java
+++ src/problem3/element/Element.java
@@ -3,4 +3,9 @@ package problem3.element;
 public class Element<T> {
     private T data;
     private int priority;
+
+    public Element(T data, int priority) {
+        this.data = data;
+        this.priority = priority;
+    }
 }
