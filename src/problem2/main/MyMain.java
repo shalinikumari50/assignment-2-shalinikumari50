d54df13b786d3bbcbde25438d94ff01d0aeb74ba
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 18:14:13 +0530

    add object:checkStatements of class: CheckStatements

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,9 +5,13 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem2.checkStatements.CheckStatements;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    CheckStatements checkStatements = new CheckStatements();
     int[][] testCasesA = {{50, 30, 20, 40, 70, 60, 80},
             {50, 60, 70, 80, 90, 99, 897, 899},
             {},
@@ -20,4 +24,5 @@ public class MyMain {
             {90, 80, 70, 60, 50, 45, 40},
             {60, 70, 80, 90, 20, 30},
             {50, 44, 60, 48, 55, 70, 32, 80, 61, 29, 18, 62, 63, 64, 65, 66, 67}};
+
 }
