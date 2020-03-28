4fb0a6cdb0e8fafa50b67ec57cf3bda29433534b
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 17:02:48 +0530

    add method: leftChildTraversal(void) to print all left children and count nodes which don't have left child

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -8,6 +8,9 @@ package problem1.mybst;
 
 import problem1.node.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
     private TreeNode root;
@@ -65,4 +68,39 @@ public class MyBinarySearchTree {
             System.out.println("No nodes in tree!");
         }
     }
+
+    public void leftChildTraversal() {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> myQueue = new LinkedList<>();
+        myQueue.add(root);
+        int NodesWithoutLeftChild = 0;
+        boolean firstLeftChild = true;
+
+        while (!myQueue.isEmpty()) {
+            TreeNode current = myQueue.remove();
+            if (current.getLeft() != null) {
+                myQueue.add(current.getLeft());
+                if (firstLeftChild) {
+                    System.out.println("All the left children in the tree are as follows:");
+                    firstLeftChild = false;
+                }
+                System.out.print(current.getLeft().getData() + " ");
+            } else {
+                NodesWithoutLeftChild++;
+            }
+            if (current.getRight() != null) {
+                myQueue.add(current.getRight());
+            }
+        }
+        if (firstLeftChild) {
+            System.out.println("Oops! There are no left children in the tree!");
+            System.out.println("Number of nodes without Left child = " + NodesWithoutLeftChild);
+        } else {
+            System.out.println("\nNumber of nodes without Left child = " + NodesWithoutLeftChild);
+        }
+    }
+
+
 }
