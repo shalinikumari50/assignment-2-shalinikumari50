94207ad43001678cc2634c25b82d60ce01d00b11
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 18:48:15 +0530

    add object: scanner of class: Scanner

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -9,11 +9,14 @@ package problem3.main;
 import problem3.myqueue.MyPriorityQueue;
 import problem5.student.Student;
 
+import java.util.Scanner;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
     public static void main(String[] args) {
         MyPriorityQueue<Student> myPriorityQueue = new MyPriorityQueue<>();
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
