fbd80c30613f9d082cc48e06fc186dc81dcf38ce
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 16:37:53 +0530

    add getter and setter methods

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -16,4 +16,28 @@ public class TreeNode {
         this.left = null;
         this.right = null;
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
