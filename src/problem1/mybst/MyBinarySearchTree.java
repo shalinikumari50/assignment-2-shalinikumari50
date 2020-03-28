079a47ea073a1d583a3c9d055068c78c5142a5e1
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-28 16:38:44 +0530

    add variable: root(TreeNode)

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,10 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    private TreeNode root;
 }
