478507682a4742d159d0de85d6c8b3967751ba83
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-03-29 21:37:25 +0530

    add static method: inputStudent(Student)

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -5,11 +5,43 @@
  *  Time: 7:06 PM
  */
 package problem5.student;
+
+import problem5.validInputs.ValidInputs;
+
 // to store student information and properties
 public class Student {
     private int rollNumber;
     private String name;
     private int backLog;
+
+    public Student(int rollNumber, String name, int backLog) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.backLog = backLog;
+    }
+
+    public static Student inputStudent() {
+        ValidInputs validInputs = new ValidInputs();
+
+        System.out.println("Enter student rollNumber");
+        int rollNumber = validInputs.inputValidPositiveInteger();
+        if (rollNumber == -1) {
+            return null;
+        }
+        System.out.println("Enter student name: ");
+        String name = validInputs.inputValidName();
+        if (name == null) {
+            return null;
+        }
+        System.out.println("Enter student's backLog: ");
+        int backLogs = validInputs.inputValidNonNegativeInteger();
+        if (backLogs == -1) {
+            return null;
+        }
+
+
+        return new Student(rollNumber, name, backLogs);
+    }
 
     public int getRollNumber() {
         return rollNumber;
@@ -35,12 +67,6 @@ public class Student {
         this.backLog = backLog;
     }
 
-    public Student(int rollNumber, String name, int backLog) {
-        this.rollNumber = rollNumber;
-        this.name = name;
-        this.backLog = backLog;
-    }
-
     @Override
     public String toString() {
         return "Student{" +
@@ -49,4 +75,5 @@ public class Student {
                 ", backLog = " + backLog +
                 '}';
     }
+
 }
